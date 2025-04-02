7552578d28b7197a1d9322b44a78c6db1d9963b3
Erick Samayoa <ericksamayoa11@gmail.com>
2025-04-01 21:27:18 -0700

    Had extra time so I tried experimenting with java and implemented some button function to actually "change text" between original display and alternate text when clicked and changed by user.

--- script.js
+++ script.js
@@ -2,11 +2,31 @@ window.onload = function () {
 
     let descriptionElements = document.querySelectorAll(".description");
 
-    descriptionElements.forEach(function(description, index) {
-        console.log('Description ${index + 1}:', description);
+    let changeTextButton = document.querySelector("#changeText");
+
+    // flag to track whether text is changed or not
+    let isChanged = false;
+    
+    //Store original text for reverting
+    let originalTexts = [];
+
+    // Capture original text on load
+    descriptionElements.forEach(function(description) {
+        originalTexts.push(description.innerText);
     });
 
-    descriptionElements.forEach(function(description) {
-        description.innerText = "This text has been changed!";
-    })
+    changeTextButton.addEventListener("click", function() {
+    descriptionElements.forEach(function(description, index) {
+        if (isChanged) {
+            //if text changes, revert to original
+            description.innerText = originalTexts[index];
+        } else {
+            // if original, change text
+            description.innerText = "Holy smokes, Batman! That worked.";
+        }
+    });
+
+    // Toggle isChanged flag
+    isChanged = !isChanged;
+    });
 };
